d219182680fef6b4a461c17728517f551173f15b
Rajarshi Bandopadhyay <theocrat93@gmail.com>
2020-04-30 18:40:22 +0530

    The viewer is now working, but only for a given test channel. TODO:   * Parse the GET query to determine which channel is to be read.   * Implement a token system to protect channels from being read by unscrupulous people   * The table schema is wrong - there should be an arrow from channels.server_id to servers.server_id Minor modifications:   * Added some lines to the .gitignore

--- viewer/format.js
+++ viewer/format.js
@@ -1,7 +1,22 @@
 function makeDocument() {
-    for(let i = 0; i < data.len; i++) {
-        document.write(`<h3>${data[i].author}</h3>`);
-        document.write(`<h4>${data[i].time}</h4>`);
-        document.write(`<p>${data[i].message}</p><hr/>`);
+    console.log('Begin makeDocument');
+    console.log(`data = ${data}`);
+    let channelArea = document.getElementById("channel-area");
+    channelArea.hidden = false;
+    if(data == null) {
+        channelArea.innerHTML += '<h1>Error processing the Database:</h1>';
+        channelArea.innerHTML += '<h2>data.json is null for some reason TwT</h2>';
+        return;
+    }
+    if(Array.isArray(data) && data.length == 0) {
+        channelArea.innerHTML += '<h1>Error processing the Database:</h1>';
+        channelArea.innerHTML += '<h2>data.json is empty for some reason TwT</h2>';
+        return;
+    }
+    for(let i = 0; i < data.length; i++) {
+        console.log("Writing message " + i);
+        channelArea.innerHTML += `<h3>${data[i].author}</h3>`;
+        channelArea.innerHTML += `<h4>${data[i].time}</h4>`;
+        channelArea.innerHTML += `<p>${data[i].message}</p><hr/>`;
     }
 }
--- viewer/queryrunner.js
+++ viewer/queryrunner.js
@@ -1,28 +1,31 @@
 const sqlite3 = require('sqlite3');
 const fs      = require('fs');
-const pmd     = require('parsemd');
+const pmd     = require('./markdown_parser');
 
-exports.queryRunner = function (dbpath, channelid) {
+exports.queryRunner = function (dbpath, channelid, callback) {
     /**
      * Runs an SQL query inside the SQLite3 database, reading all messages from a given channel.
      * Reads `message, `author and `time fields from the channel.
-     * Then opens the file `data.json` and prints the object as a JSON string.
+     * Then opens the file `data.js` and prints the object as a JSON string.
      * Formats the message with parsemd first.
      */
     let db = new sqlite3.Database(dbpath, function (err) {
-        console.error('Could not open database!');
-        fs.writeFile('data.json', 'data = null', function (err) {
-            if(err) {
-                console.error("Wow! I can't even write the error file!");
-                console.error("This is going to lead to some nasty results on the webpage...");
-            }
-        });
-        return ;
+        if(err) {
+            console.error('Could not open database!');
+            console.error('Error: ' + err);
+            fs.writeFile('data.js', 'data = null', function (err) {
+                if(err) {
+                    console.error("Wow! I can't even write the error file!");
+                    console.error("This is going to lead to some nasty results on the webpage...");
+                }
+            });
+            return ;
+        }
     });
     db.all('select * from messages where channel_id = ' + channelid, function (err, obj) {
         if(err) {
             console.error(err);
-            fs.writeFile('data.json', 'data = null', function (err) {
+            fs.writeFile('data.js', 'data = null', function (err) {
                 if(err) {
                     console.error("Wow! I can't even write the error file!");
                     console.error("This is going to lead to some nasty results on the webpage...");
@@ -33,13 +36,13 @@ exports.queryRunner = function (dbpath, channelid) {
             console.log(`Read ${obj.length} lines form the DB`);
             for(let i = 0; i < obj.length; i++) {
                 row = obj[i];
-                row.message = await pmd.parsemd(row.message);
+                row.message = pmd.parsemd(row.message);
                 obj[i] = row;
             }
-            let jstr = 'data = ' + obj.stringify();
-            fs.writeFile('data.json', jstr, function (err) {
+            let jstr = 'data = ' + JSON.stringify(obj);
+            fs.writeFile('data.js', jstr, function (err) {
                 if(err) {
-                    console.error("Welp! Can't write to the data.json file.");
+                    console.error("Welp! Can't write to the data.js file.");
                     console.error("This is going to lead to some nasty results on the webpage...");
                 }
             });
